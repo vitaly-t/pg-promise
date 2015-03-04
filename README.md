ff34a373016b068f65a83cd5cecb283790d797bc
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-03-04 20:45:47 +0000

    Bringing it in sync with the NPM code.
    Signed-off-by: Vitaly Tomilov <vitaly.tomilov@gmail.com>

--- README.md
+++ README.md
@@ -1,9 +1,174 @@
-## Introduction
+# Introduction
 
-Following a large-scale project, I had to implement a good wrapper for PG package,
-to support Promise object for inside and outside transactions, as no other library
-seems to offer a simple and concise approach.
+This library unifies [Promise] and [PG] to fascilitate writing easy-to-read database code that relies on promises:
+* Simplistic approach to organizing streamlined database code, thanks to full [Promise] integration;
+* Database connections are managed automatically, in every usage case;
+* Functions, Procedures and Transactions are all fully supported;
+* Robust approach to handling results from every single query.
 
-## Log
-March 3rd, 2015.
-I just started working on it, changing to be more suitable for public consumption.
+# Getting started
+
+### 1. Load the library
+````
+var pgpLib = require('pg-promise');
+````
+### 2. Configure database connection
+````
+var config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'my_db_name',
+    user: 'postgres',
+    password: 'bla-bla'
+};
+````
+The library itself doesn't use this object at all, just passing it on to PG to interpret and use (see ````ConnectionParameters```` in [PG] package for details).
+This also means that you can pass a connection string instead, it is up to [PG] then to figure what it is and process accordingly.
+
+For example, the same ````config```` could be defined as this instead:
+````
+var config = "postgres://postgres:bla-bla@localhost/my_db_name";
+````
+It would work exactly the same, though perhaps it then then should be renamed into ````cnString````.
+
+### 3. Initialize the library
+````
+var pgp = pgpLib(config);
+````
+Only one global instance should be used as shown above. Now you are ready to use it.
+
+For additional use of initialization ```options``` see chapter Advanced.
+
+# Usage
+### The basics
+In order to eliminate the chances of unexpected query results and make code more robust, each request is parametrized with the expected/supported return result mask, using type ````queryResult```` as shown below:
+````
+queryResult = {
+    one: 1,     // single-row result is expected;
+    many: 2,    // multi-row result is expected;
+    none: 4     // no rows expected.
+};
+````
+In the following generic-query example we indicate that the call must return either no records or multiple records:
+````
+pgp.query("select * from users", queryResult.none | queryResult.many);
+````
+This usage pattern is facilitated through result-specific methods that can be used instead of the generic query:
+````
+pgp.many("select * from users"); // multiple records are expected
+pgp.one("select * from users limit 1"); // one record is expected
+pgp.none("update users set active=TRUE where id=1"); // no records expected
+````
+There are also mixed-result ones, called ````oneOrNone```` and ````manyOrNone````.
+
+Each of the query calls returns a [Promise] object, as shown below, to be used in the standard way. And when the expected and actual results do not match, the call will be rejected.
+````
+pgp.many("select * from users").then(function(data){
+    console.log(data); // printing the data received
+},function(reason){
+    console.log(reason); // printing the reason why the call was rejected
+});
+````
+### Functions and Procedures
+In PostgreSQL stored procedures are just functions that usually do not return anything.
+
+Suppose we want to call a function to find audit records by user id and maximum time stamp. We can call a corresponding function as shown below:
+````
+pgp.func('findAudit', [
+    123,
+    new Date()
+]);
+````
+We passed it user Id = 123, plus current Date/Time as the time stamp. We assume that the function signature matches the parameters that we passed. All values passed are serialized automatically to comply with PostgreSQL requirements.
+
+In the same way you can call ````pgp.proc````, which doesn't care about the returned result. Both methods return a promise object.
+
+### Transactions
+Every call shown in chapters above would acquire a new connection from the pool and release it when done. In order to execute a transaction on the same connection, a transaction class is to be used.
+
+Example:
+````
+var promise = require('promise');
+
+var tx = new pgp.tx(); // creating a new transaction object
+tx.exec(function(){
+    return promise.all([
+        tx.none("update users set active=TRUE"),
+        tx.one("insert into audit(event) values('user changed') returning id")
+    ]);
+}).then(function(data){
+    console.log(data); // printing successful transation output
+}, function(reason){
+    console.log(reason); // printing the reason why the transaction was rejected
+});
+````
+In the example above we create a new transaction object and call its method ````exec````, passing it a call-back function that must do all the queries needed and return a [Promise] object. In the example we use ````promise.all```` to indicate that we want both queries inside the transaction to succeed to consider it a success; otherwise the transaction is to be rolled back.
+
+<b>Notes</b>
+1. While inside a transaction, we make calls to the same-named methods as outside of transactions, except we do it on the transaction object instance now, as opposed to the global ````pgp```` object, which gives us access to the shared connection object. The same goes for calling functions and procedures within transactions, using ````tx.func```` and ````tx.proc```` accordingly.
+2. Just for flexibility, the transaction call-back function takes one parameter - PG client connection object.
+
+### Type Helpers
+The library provides several helper functions to convert a basic javascript type into its proper PostgreSQL presentation that can be passed directly into queries or functions as parameters.
+All of such helper functions are located within namespace ````pgp.as````:
+````
+pgp.as.bool(value); // returns proper postgresql boolean presentation
+
+pgp.as.text(value); // returns proper postgresql text presentation,
+                    // fixing single-quote symbols
+
+pgp.as.date(value); // returns proper postgresql date/time presentation
+````
+As these helpers are not associated with a connection, they can be called from inside or outside a transaction.
+
+# Advanced
+
+### Initialization options
+Initialization options are supported as shown in the example below:
+```
+var options = {
+    connect: function(client){
+        console.log('New virtual connection established');
+    },
+    disconnect: function(client){
+        console.log('Virtual connection released');
+    }
+};
+var pgp = pgpLib(config, options);
+```
+Two events supported at the moment - ```connect``` and ```disconnect```, to notify of virtual connections being established or closed accordingly. Each takes parameter ```client```, which is the client connection object.
+
+### De-initialization
+When exiting your application, make the following call:
+```
+pgp.end();
+```
+This will release pg connection pool globally and make sure that the process terminates without delay. If you do not call it, your process may be waiting for 30 seconds (default) or so, waiting for the pg connection pool to expire.
+
+### Direct connection usage
+The library exposes method ```connect``` in case for some unique reason you want to be able to open and close the connection yourself, as opposed to trusting the library doing it for you automatically.
+
+Usage example:
+```
+pgp.connect().then(function(db){
+    // connection was established successfully;
+
+    // do stuff with the connection object (db.client) and/or queries;
+
+    // when done with all the queries, call done():
+    db.done();
+
+}, function(reason){
+    // failed to connect;
+    console.log('Connection problem: ' + reason);
+});
+```
+<b>NOTE:</b> When using the direct connection, events ```connect``` and ```disconnect``` won't be fired.
+
+# History
+* It reached first Beta version 0.1.0 on March 4th, 2015.
+* The first draft v0.0.1 was published on March 3rd, 2015, and then rapidly incremented due to many initial changes that had to come in, mostly documentation.
+
+[PG]:https://www.npmjs.com/package/pg
+[Promise]:https://www.npmjs.com/package/promise
+
--- index.js
+++ index.js
@@ -1,123 +1,255 @@
-'use strict';
-
 var npm = {
     promise: require('promise'),
     pg: require('pg')
 };
 
-var queryResult = {
-    one: 1,
-    many: 2,
-    none: 4
+//////////////////////////////////////
+// Query result mask flags;
+//
+// NOTE: Cannot combine one + many, while the
+// rest of combinations are all supported.
+queryResult = {
+    one: 1,     // single-row result is expected;
+    many: 2,    // multi-row result is expected;
+    none: 4     // no rows expected.
 };
 
-exports.PGPromise = function (config, options) {
+////////////////////////////////////////////////
+// Main entry function;
+//
+// Parameters:
+//
+// 1. config (required) - either configuration
+//    object or connection string. Either way,
+//    it is merely passed on to PG and not used
+//    by this library in any way.
+// 2. options (optional) - object with optional
+//    configuration properties:
+//    {
+//       connect: function(client){
+//           connection-notify event;
+//           client - pg connection object.
+//       },
+//       disconnect: function(client){
+//           disconnection-notify event;
+//           client - pg connection object.
+//       }
+//    }
+module.exports = function (config, options) {
 
     if (!config) {
         throw new Error("Invalid 'config' parameter passed.");
     }
-
-    // IMPORTANT: The caller must invoke done() and then client.end() when finished.
-    this.connect = function () {
-        return $p(function (resolve, reject) {
-            npm.pg.connect(config, function (err, client, done) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve({
-                        client: client,
-                        done: done
-                    });
-                }
-            });
-        });
-    };
-
-    // Simple query, with opening and closing connection;
-    this.query = function (query, qr) {
-        return $p(function (resolve, reject) {
-            $self.connect()
-                .then(function (db) {
-                    $prop.monitor(true, db);
-                    $prop.query(db.client, query, qr)
-                        .then(function (data) {
-                            $prop.monitor(false, db);
-                            resolve(data);
-                        }, function (reason) {
-                            $prop.monitor(false, db);
-                            reject(reason);
-                        });
-                }, function (reason) {
-                    reject(reason); // connection failed;
-                });
-        });
-    };
-
-    this.none = function (query) {
-        return this.query(query, queryResult.none);
-    };
-
-    this.one = function (query) {
-        return this.query(query, queryResult.one);
-    };
-
-    this.many = function (query) {
-        return this.query(query, queryResult.many);
-    };
-
-    this.oneOrNone = function (query) {
-        return this.query(query, queryResult.one | queryResult.none);
-    };
-
-    this.manyOrNone = function (query) {
-        return this.query(query, queryResult.many | queryResult.none);
-    };
-
-    this.func = function (funcName, params) {
-        return this.one($prop.createFuncQuery(funcName, params));
-    };
-
-    this.proc = function (procName, params) {
-        return this.oneOrNone($prop.createFuncQuery(procName, params));
-    };
-
-    this.as = {
-        bool: function (val) {
-            if ($prop.isNull(val)) {
-                return 'null';
-            }
-            return val ? 'TRUE' : 'FALSE';
-        },
-        text: function (val) {
-            if ($prop.isNull(val)) {
-                return 'null';
-            }
-            return $prop.wrapText($prop.fixQuotes(val));
-        },
-        date: function (val) {
-            if ($prop.isNull(val)) {
-                return 'null';
-            }
-            if (val instanceof Date) {
-                return $prop.wrapText(val.toUTCString());
-            } else {
-                throw new Error($prop.wrapText(val) + " doesn't represent a valid Date object or value");
-            }
-        }
-    };
 
     // new promise initializer;
     var $p = function (func) {
         return new npm.promise(func);
     };
 
-    // self-pointer;
-    var $self = this;
+    var $self = {
+        // IMPORTANT: The caller must invoke done() after requests are finished.
+        connect: function () {
+            return $p(function (resolve, reject) {
+                npm.pg.connect(config, function (err, client, done) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({
+                            client: client,
+                            done: done
+                        });
+                    }
+                });
+            });
+        },
+        end: function(){
+            npm.pg.end(); // Terminates pg library;
+        },
+        // Simple query, with opening and closing connection;
+        query: function (query, qr) {
+            return $p(function (resolve, reject) {
+                $self.connect()
+                    .then(function (db) {
+                        $prop.monitor(true, db);
+                        $prop.query(db.client, query, qr)
+                            .then(function (data) {
+                                $prop.monitor(false, db);
+                                resolve(data);
+                            }, function (reason) {
+                                $prop.monitor(false, db);
+                                reject(reason);
+                            });
+                    }, function (reason) {
+                        reject(reason); // connection failed;
+                    });
+            });
+        },
+
+        none: function (query) {
+            return this.query(query, queryResult.none);
+        },
+
+        one: function (query) {
+            return this.query(query, queryResult.one);
+        },
+
+        many: function (query) {
+            return this.query(query, queryResult.many);
+        },
+
+        oneOrNone: function (query) {
+            return this.query(query, queryResult.one | queryResult.none);
+        },
+
+        manyOrNone: function (query) {
+            return this.query(query, queryResult.many | queryResult.none);
+        },
+
+        func: function (funcName, params) {
+            return this.one($prop.createFuncQuery(funcName, params));
+        },
+
+        proc: function (procName, params) {
+            return this.oneOrNone($prop.createFuncQuery(procName, params));
+        },
+
+        as: {
+            bool: function (val) {
+                if ($prop.isNull(val)) {
+                    return 'null';
+                }
+                return val ? 'TRUE' : 'FALSE';
+            },
+            text: function (val) {
+                if ($prop.isNull(val)) {
+                    return 'null';
+                }
+                return $prop.wrapText($prop.fixQuotes(val));
+            },
+            date: function (val) {
+                if ($prop.isNull(val)) {
+                    return 'null';
+                }
+                if (val instanceof Date) {
+                    return $prop.wrapText(val.toUTCString());
+                } else {
+                    throw new Error($prop.wrapText(val) + " doesn't represent a valid Date object or value");
+                }
+            }
+        },
+
+        tx: function () {
+
+            var tx = this;
+
+            tx.exec = function (cb) {
+                if (tx.$prop.db) {
+                    throw new Error('Cannot execute an unfinished transaction');
+                }
+                var t_data, t_reason, success;
+                return $p(function (resolve, reject) {
+                    $self.connect()
+                        .then(function (db) {
+                            tx.$prop.start(db);
+                            return tx.query('begin', queryResult.none);
+                        }, function (reason) {
+                            reject(reason); // connection issue;
+                        })
+                        .then(function () {
+                            tx.$prop.call(cb)
+                                .then(function (data) {
+                                    t_data = data;
+                                    success = true;
+                                    return tx.query('commit', queryResult.none);
+                                }, function (reason) {
+                                    t_reason = reason;
+                                    success = false;
+                                    return tx.query('rollback', queryResult.none);
+                                })
+                                .then(function () {
+                                    tx.$prop.finish();
+                                    // either commit or rollback successfully executed;
+                                    if (success) {
+                                        resolve(t_data);
+                                    } else {
+                                        reject(t_reason);
+                                    }
+                                }, function (reason) {
+                                    // either commit or rollback failed;
+                                    tx.$prop.finish();
+                                    reject(reason);
+                                });
+                        }, function (reason) {
+                            tx.$prop.finish();
+                            reject(reason); // issue with 'begin' command;
+                        });
+                });
+            };
+
+            tx.query = function (query, qr) {
+                if (!tx.$prop.db) {
+                    throw new Error('Unexpected call outside of transaction');
+                }
+                return $prop.query(tx.$prop.db.client, query, qr);
+            };
+
+            tx.none = function (query) {
+                return tx.query(query, queryResult.none);
+            };
+
+            tx.one = function (query) {
+                return tx.query(query, queryResult.one);
+            };
+
+            tx.many = function (query) {
+                return tx.query(query, queryResult.many);
+            };
+
+            tx.oneOrNone = function (query) {
+                return tx.query(query, queryResult.one | queryResult.none);
+            };
+
+            tx.manyOrNone = function (query) {
+                return tx.query(query, queryResult.many | queryResult.none);
+            };
+
+            tx.func = function (funcName, params) {
+                return tx.one($prop.createFuncQuery(funcName, params));
+            };
+
+            tx.proc = function (procName, params) {
+                return tx.oneOrNone($prop.createFuncQuery(procName, params));
+            };
+
+            // private properties;
+            tx.$prop = {
+                db: null,
+                start: function (db) {
+                    this.db = db;
+                    $prop.monitor(true, db);
+                },
+                finish: function () {
+                    $prop.monitor(false, this.db);
+                    this.db = null;
+                },
+                call: function (cb) {
+                    if (typeof(cb) !== 'function') {
+                        return npm.promise.reject("Cannot invoke tx.exec() without a callback function.");
+                    }
+                    var result = cb(this.db.client);
+                    if (result && typeof(result.then) === 'function') {
+                        return result;
+                    } else {
+                        return npm.promise.reject("Callback function passed into tx.exec() didn't return a valid promise object.");
+                    }
+                }
+            };
+        }
+    };
 
     // private properties and functions;
     var $prop = {
         options: options,
-        cnCounter: 0, // internal connection counter;
         isNull: function (val) {
             return typeof(val) === 'undefined' || val === null;
         },
@@ -146,31 +278,26 @@ exports.PGPromise = function (config, options) {
         },
         monitor: function (open, db) {
             if (open) {
-                this.cnCounter++;
                 if (this.options) {
                     var func = this.options.connect;
                     if (func) {
                         if (typeof(func) !== 'function') {
                             throw new Error('Function was expected for options.connect');
                         }
-                        func(this.cnCounter, db.client);
+                        func(db.client);
                     }
                 }
             } else {
-                this.cnCounter--;
                 if (this.options) {
                     var func = this.options.disconnect;
                     if (func) {
                         if (typeof(func) !== 'function') {
                             throw new Error('Function was expected for options.disconnect');
                         }
-                        func(this.cnCounter, db.client);
+                        func(db.client);
                     }
                 }
                 db.done();
-                if (this.cnCounter === 0) {
-                    db.client.end();
-                }
             }
         },
         formatValues: function (values) {
@@ -198,7 +325,7 @@ exports.PGPromise = function (config, options) {
                         var l = result.rows.length;
                         if (l) {
                             if (l > 1 && !(qr & queryResult.many)) {
-                                reject('Single record was expected from query: ' + query);
+                                reject("Single record was expected from query: '" + query + "'");
                             } else {
                                 if (!(qr & queryResult.many)) {
                                     data = result.rows[0];
@@ -208,7 +335,7 @@ exports.PGPromise = function (config, options) {
                             if (qr & queryResult.none) {
                                 data = null;
                             } else {
-                                reject('No records returned from query: ' + query);
+                                reject("No records returned from query: '" + query + "'");
                             }
                         }
                         resolve(data);
@@ -218,108 +345,5 @@ exports.PGPromise = function (config, options) {
         }
     };
 
-    this.tx = function () {
-
-        var tx = this;
-
-        tx.exec = function (cb) {
-            if (tx.$prop.db) {
-                throw new Error('Cannot execute an unfinished transaction');
-            }
-            var t_data, t_reason, success;
-            return $p(function (resolve, reject) {
-                $self.connect()
-                    .then(function (db) {
-                        tx.$prop.start(db);
-                        return tx.query('begin', queryResult.none);
-                    }, function (reason) {
-                        reject(reason); // connection issue;
-                    })
-                    .then(function () {
-                        tx.$prop.call(cb)
-                            .then(function (data) {
-                                t_data = data;
-                                success = true;
-                                return tx.query('commit', queryResult.none);
-                            }, function (reason) {
-                                t_reason = reason;
-                                success = false;
-                                return tx.query('rollback', queryResult.none);
-                            })
-                            .then(function () {
-                                tx.$prop.finish();
-                                // either commit or rollback successfully executed;
-                                if (success) {
-                                    resolve(t_data);
-                                } else {
-                                    reject(t_reason);
-                                }
-                            }, function (reason) {
-                                // either commit or rollback failed;
-                                tx.$prop.finish();
-                                reject(reason);
-                            });
-                    }, function (reason) {
-                        tx.$prop.finish();
-                        reject(reason); // issue with 'begin' command;
-                    });
-            });
-        };
-
-        tx.query = function (query, qr) {
-            if (!tx.$prop.db) {
-                throw new Error('Unexpected call outside of transaction');
-            }
-            return $prop.query(tx.$prop.db.client, query, qr);
-        };
-
-        tx.none = function (query) {
-            return tx.query(query, queryResult.none);
-        };
-
-        tx.one = function (query) {
-            return tx.query(query, queryResult.one);
-        };
-
-        tx.many = function (query) {
-            return tx.query(query, queryResult.many);
-        };
-
-        tx.oneOrNone = function (query) {
-            return tx.query(query, queryResult.one | queryResult.none);
-        };
-
-        tx.manyOrNone = function (query) {
-            return tx.query(query, queryResult.many | queryResult.none);
-        };
-
-        tx.func = function (funcName, params) {
-            return tx.one($prop.createFuncQuery(funcName, params));
-        };
-
-        tx.proc = function (procName, params) {
-            return tx.oneOrNone($prop.createFuncQuery(procName, params));
-        };
-
-        // private properties;
-        tx.$prop = {
-            db: null,
-            start: function (db) {
-                this.db = db;
-                $prop.monitor(true, db);
-            },
-            finish: function () {
-                $prop.monitor(false, this.db);
-                this.db = null;
-            },
-            call: function(cb){
-                var result = cb(this.db.client);
-                if(result && typeof(result.then) === 'function'){
-                    return result;
-                }else{
-                    return npm.promise.reject("Callback function passed into tx.exec() didn't return a valid promise object.");
-                }
-            }
-        };
-    };
+    return $self;
 };
