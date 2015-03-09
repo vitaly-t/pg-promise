a639d06485447471846f3402f1d8b80818743648
Vitaly Tomilov <vtomilov@deloitte.ie>
2015-03-09 12:29:26 +0000

    test integration.

---
+++ Makefile
@@ -0,0 +1,4 @@
+test:
+	@./node_modules/jasmine-node/bin/jasmine-node test
+
+.PHONY: test
---
+++ test/default.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("Test", function () {
+    it("test", function () {
+        // TODO : Test codes will be written
+    });
+});
