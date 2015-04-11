cfb569b9575a991015c7cb823849283a387191a8
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-04-11 12:18:02 +0100

    improving test initialization.

--- test/db/init.js
+++ test/db/init.js
@@ -1,6 +1,9 @@
+////////////////////////////////////////////////
 // Initialization scripts for the test database;
+////////////////////////////////////////////////
 
 var promise = require('bluebird');
+
 var dbHeader = require('./header')({
     query: function (e) {
         console.log(e.query); // print all of the queries;
@@ -12,31 +15,38 @@ var db = dbHeader.db;
 
 (function () {
 
-    db.tx(function (ctx) {
+    db.tx(function (t) {
         return promise.all([
-            ctx.none("drop table if exists audit"),
-            ctx.none("drop table if exists person"),
-            ctx.none("drop table if exists users"),
-            ctx.none("drop table if exists images"),
 
-            ctx.none("create table audit(id serial, event text, created timestamptz, ref int)"),
-            ctx.none("create table person(id serial, name text, dob date)"),
-            ctx.none("create table users(id serial, login text, active boolean)"),
-            ctx.none("create table images(id serial, name text, data bytea)")
+            // drop all tables;
+            t.none("drop table if exists audit"),
+            t.none("drop table if exists person"),
+            t.none("drop table if exists users"),
+            t.none("drop table if exists images"),
+
+            // create all tables;
+            t.none("create table audit(id serial, event text, created timestamptz, ref int)"),
+            t.none("create table person(id serial, name text, dob date)"),
+            t.none("create table users(id serial, login text, active boolean)"),
+            t.none("create table images(id serial, name text, data bytea)"),
+
+            // insert records into 'users';
+            t.none("insert into users(login, active) values($1,$2)", ['user-1', true]),
+            t.none("insert into users(login, active) values($1,$2)", ['user-2', true]),
+            t.none("insert into users(login, active) values($1,$2)", ['user-3', false]),
+            t.none("insert into users(login, active) values($1,$2)", ['user-4', false]),
+
+            // insert records into 'person';
+            t.none("insert into person(name, dob) values($1,$2::date)", ['David', new Date(1995, 08, 7)]),
+            t.none("insert into person(name, dob) values($1,$2::date)", ['John', new Date(1980, 03, 20)]),
+            t.none("insert into person(name, dob) values($1,$2::date)", ['Mark', new Date(1973, 05, 12)]),
+            t.none("insert into person(name, dob) values($1,$2::date)", ['Peter', new Date(1992, 11, 3)])
         ]);
     })
         .then(function () {
-            return db.tx(function (ctx) {
-                return promise.all([
-                    ctx.none("insert into users(login, active) values($1,$2)", ['user-1', true]),
-                    ctx.none("insert into users(login, active) values($1,$2)", ['user-2', true]),
-                    ctx.none("insert into users(login, active) values($1,$2)", ['user-3', false]),
-
-                    ctx.none("insert into person(name, dob) values($1,$2::date)", ['John', new Date(1980, 03, 20)]),
-                    ctx.none("insert into person(name, dob) values($1,$2::date)", ['Mark', new Date(1973, 05, 12)]),
-                    ctx.none("insert into person(name, dob) values($1,$2::date)", ['Peter', new Date(1992, 11, 3)])
-                ]);
-            });
+            console.log("\r\n*** SUCCESS ***");
+        }, function (reason) {
+            console.log("\r\n*** FAILED: " + reason);
         })
         .done(function () {
             pgp.end();
