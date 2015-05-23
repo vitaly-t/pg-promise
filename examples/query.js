f6c762603736249c8a4f3bebfb1eb0f9e0f409f7
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-05-23 06:31:30 +0100

    improving examples.

--- examples/query.js
+++ examples/query.js
@@ -1,18 +1,21 @@
-///////////////////////////////////////////////////////////
-// This is to show a complete example of using the library;
-///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+// This is to show a complete application when using the library;
+/////////////////////////////////////////////////////////////////
 
-// Loading and initializing the library without options:
-var pgp = require('pg-promise')();
+// Loading and initializing the library without options;
+// See also: https://github.com/vitaly-t/pg-promise#initialization-options
+var pgp = require('pg-promise')(/*options*/);
 
-// Database connection details:
+// Database connection details;
 var cn = {
-    host: 'localhost',
-    port: 5432,
+    host: 'localhost', // 'localhost' is the default;
+    port: 5432, // 5432 is the default;
     database: 'myDatabase',
     user: 'myUser',
     password: 'myPassword'
 };
+// You can check for default values in:
+// https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
 
 var db = pgp(cn); // database instance;
 
@@ -24,10 +27,13 @@ db.query("select * from users where active=$1", true)
     })
     .done(function () {
         // If we do not close the connection pool when exiting the application,
-        // it may take 30 seconds or so before the process terminates, waiting
-        // for the connection pool to expire.
+        // it may take 30 seconds (poolIdleTimeout) before the process terminates,
+        // waiting for the connection to expire in the pool.
 
         // But if you normally just kill the process, then it doesn't matter.
 
-        pgp.end(); // closing the connection pool, to exit immediately.
+        pgp.end(); // destroying the connection pool, to exit immediately.
+
+        // See also:
+        // https://github.com/vitaly-t/pg-promise#library-de-initialization
     });
