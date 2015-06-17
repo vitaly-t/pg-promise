6171862b7814b59cd8c34301af8da2cfb1b27d39
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-06-18 00:20:18 +0100

    updated the test file.

--- test.bat
+++ test.bat
@@ -3,4 +3,8 @@
 REM Use this batch to run tests on Windows.
 REM NOTE: Make sure to follow all the Testing steps first: https://github.com/vitaly-t/pg-promise#testing
 
-node_modules/.bin/jasmine-node.cmd test
+REM node_modules/.bin/jasmine-node.cmd test
+
+node_modules/.bin/jasmine.cmd
+
+
