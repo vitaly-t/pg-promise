e9838d1a3e6b98493a1e4c280da5d4dd5b1cbd1b
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-06-28 13:34:33 +0100

    updating init script.

--- test/db/init.js
+++ test/db/init.js
@@ -2,53 +2,54 @@
 // Initialization scripts for the test database;
 ////////////////////////////////////////////////
 
-var promise = require('bluebird');
-
-var dbHeader = require('./header')({
+var dbHeader = require('./header');
+var promise = dbHeader.promise;
+var header = dbHeader({
     query: function (e) {
         console.log(e.query); // print all of the queries;
-    }
+    },
+    promiseLib: promise
 });
 
-var pgp = dbHeader.pgp;
-var db = dbHeader.db;
+var pgp = header.pgp;
+var db = header.db;
 
 (function () {
 
-    db.tx(function (t) {
+    db.tx(function () {
         return promise.all([
 
             // drop all functions;
-            t.none("drop function if exists findUser(int)"),
-            t.none("drop function if exists getUsers()"),
+            this.none("drop function if exists findUser(int)"),
+            this.none("drop function if exists getUsers()"),
 
             // drop all tables;
-            t.none("drop table if exists audit"),
-            t.none("drop table if exists person"),
-            t.none("drop table if exists users"),
-            t.none("drop table if exists images"),
+            this.none("drop table if exists audit"),
+            this.none("drop table if exists person"),
+            this.none("drop table if exists users"),
+            this.none("drop table if exists images"),
 
             // create all tables;
-            t.none("create table audit(id serial, event text, created timestamptz, ref int)"),
-            t.none("create table person(id serial, name text, dob date)"),
-            t.none("create table users(id serial, login text, active boolean)"),
-            t.none("create table images(id serial, name text, data bytea)"),
+            this.none("create table audit(id serial, event text, created timestamptz, ref int)"),
+            this.none("create table person(id serial, name text, dob date)"),
+            this.none("create table users(id serial, login text, active boolean)"),
+            this.none("create table images(id serial, name text, data bytea)"),
 
             // insert records into 'users';
-            t.none("insert into users(login, active) values($1,$2)", ['user-1', true]),
-            t.none("insert into users(login, active) values($1,$2)", ['user-2', true]),
-            t.none("insert into users(login, active) values($1,$2)", ['user-3', false]),
-            t.none("insert into users(login, active) values($1,$2)", ['user-4', false]),
+            this.none("insert into users(login, active) values($1,$2)", ['user-1', true]),
+            this.none("insert into users(login, active) values($1,$2)", ['user-2', true]),
+            this.none("insert into users(login, active) values($1,$2)", ['user-3', false]),
+            this.none("insert into users(login, active) values($1,$2)", ['user-4', false]),
 
             // insert records into 'person';
-            t.none("insert into person(name, dob) values($1,$2::date)", ['David', new Date(1995, 08, 7)]),
-            t.none("insert into person(name, dob) values($1,$2::date)", ['John', new Date(1980, 03, 20)]),
-            t.none("insert into person(name, dob) values($1,$2::date)", ['Mark', new Date(1973, 05, 12)]),
-            t.none("insert into person(name, dob) values($1,$2::date)", ['Peter', new Date(1992, 11, 3)]),
+            this.none("insert into person(name, dob) values($1,$2::date)", ['David', new Date(1995, 08, 7)]),
+            this.none("insert into person(name, dob) values($1,$2::date)", ['John', new Date(1980, 03, 20)]),
+            this.none("insert into person(name, dob) values($1,$2::date)", ['Mark', new Date(1973, 05, 12)]),
+            this.none("insert into person(name, dob) values($1,$2::date)", ['Peter', new Date(1992, 11, 3)]),
 
             // adding functions;
-            t.none("create or replace function findUser(userId int) returns setof users as $$ select * from users where id = userId $$ language 'sql';"),
-            t.none("create or replace function getUsers() returns setof users as $$ select * from users $$ language 'sql';")
+            this.none("create or replace function findUser(userId int) returns setof users as $$ select * from users where id = userId $$ language 'sql';"),
+            this.none("create or replace function getUsers() returns setof users as $$ select * from users $$ language 'sql';")
         ]);
     })
         .then(function () {
