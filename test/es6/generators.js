6053b066f29fb69ca1461b56ad75cc01b0769856
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-11-30 09:07:02 +0000

    addign generators + special es6 builds + tests.

---
+++ test/es6/generators.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var header = require('../db/header');
+var promise = header.defPromise;
+var dbHeader = header();
+var db = dbHeader.db;
+
+describe("Generators - Positive", function () {
+
+    var result;
+
+    function* myTask() {
+        return yield promise.resolve(123);
+    }
+
+    beforeEach(function (done) {
+        db.task(myTask)
+            .then(function (data) {
+                result = data;
+                done();
+            });
+    });
+
+    it("must resolve with the right value", function () {
+        expect(result).toBe(123);
+    });
+});
+
+describe("Generators - Negative", function () {
+
+    describe("normal reject", function () {
+        var result;
+
+        function* myTask() {
+            return yield promise.reject(123);
+        }
+
+        beforeEach(function (done) {
+            db.task(myTask)
+                .catch(function (error) {
+                    result = error;
+                    done();
+                });
+        });
+
+        it("must reject with the right value", function () {
+            expect(result).toBe(123);
+        });
+    });
+
+    describe("error thrown", function () {
+
+        var result;
+
+        function* myTask() {
+            throw 123;
+        }
+
+        beforeEach(function (done) {
+            db.task(myTask)
+                .catch(function (error) {
+                    result = error;
+                    done();
+                });
+        });
+
+        it("must reject with the right value", function () {
+            expect(result).toBe(123);
+        });
+
+    });
+});
