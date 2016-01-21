93a1cbbf1a5ea1772bf64c164244035647d93029
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-01-21 10:59:59 +0000

    downgrading postgresql back to 9.4, updating docs.

--- jsdoc/fixLinks.js
+++ jsdoc/fixLinks.js
@@ -12,8 +12,7 @@ var links = {
     "pg.Client": "https://github.com/brianc/node-postgres/wiki/Client",
     "BEGIN": "http://www.postgresql.org/docs/9.5/static/sql-begin.html",
     "Transaction Isolation": "http://www.postgresql.org/docs/9.5/static/transaction-iso.html",
-    "pg-minify": "https://github.com/vitaly-t/pg-minify",
-    "SQLParsingError": "https://github.com/vitaly-t/pg-minify/blob/master/lib/error.js"
+    "pg-minify": "https://github.com/vitaly-t/pg-minify"
 };
 
 function fixLinks(source) {
