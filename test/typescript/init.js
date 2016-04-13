d3e0105c83619d2d9ee08284231355df8782704c
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-04-13 17:39:53 +0100

    deleting files.

--- test/typescript/init.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../pg-promise.d.ts" />
-"use strict";
-const lib = require("pg-promise");
-var pgp = lib({
-    capSQL: true,
-    pgFormatting: true,
-    pgNative: true
-});
-var db = pgp('connection');
