0ca275ffee51292683c60e7ae5bad68dfd4edc0c
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-05-23 02:18:44 +0100

    upped the version.

--- typescript/pg-promise.d.ts
+++ typescript/pg-promise.d.ts
@@ -1,5 +1,5 @@
 ////////////////////////////////////////
-// Requires pg-promise v4.2.0 or later.
+// Requires pg-promise v4.2.8 or later.
 ////////////////////////////////////////
 
 /// <reference path='./pg-subset' />
