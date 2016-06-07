vitaly-t/pg-promise

commit 9ea93854a791f8f9289e90b71b0bfb86f67a338f

adding support for Configuration Path.

lib/utils/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var $path = require('path');
+
 ////////////////////////////////////////////
 // Simpler check for null/undefined;
 function isNull(value) {
@@ -104,6 +106,7 @@ function inherits(child, parent) {
 }
 
 module.exports = {
+    startDir: $path.dirname(process.argv[1]),
     lock: lock,
     isText: isText,
     isNull: isNull,
@@ -115,3 +118,6 @@ module.exports = {
     messageGap: messageGap,
     inherits: inherits
 };
+
+Object.freeze(module.exports);
+

lib/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var $npm = {
+    path: require('path'),
     pg: require('pg'),
     minify: require('pg-minify'),
     array: require('./array'),
@@ -176,12 +177,7 @@ function $main(options) {
     var Database = require('./database')(config);
 
     var inst = function (cn, dc) {
-        var t = typeof cn;
-        if (cn && (t === 'string' || t === 'object')) {
-            return new Database(cn, dc, config);
-        }
-        // cannot access a database without connection details;
-        throw new TypeError("Invalid connection details.");
+        return new Database(parseConnection(cn), dc, config);
     };
 
     $npm.utils.addReadProperties(inst, rootNameSpace);
@@ -223,6 +219,21 @@ function $main(options) {
     $npm.utils.addReadProp(inst, 'helpers', $npm.helpers(config));
 
     return inst;
+}
+
+function parseConnection(cn) {
+    if (cn) {
+        if ($npm.utils.isText(cn)) {
+            if (cn.indexOf('://') !== -1) {
+                return cn;
+            }
+            cn = require($npm.path.join($npm.utils.startDir, cn));
+        }
+        if (typeof cn === 'object') {
+            return cn;
+        }
+    }
+    throw new TypeError("Invalid connection details.");
 }
 
 var rootNameSpace = {

lib/database.js
@@ -21,7 +21,13 @@ var $arr = require('./array');
  * This type is not available directly, it can only be created via the library's base call.
  *
  * @param {String|Object} cn
- * Connection object or string. For details see {@link https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax Connection Syntax}.
+ * Database connection details, which can be any of the following:
+ * 
+ * - a configuration object
+ * - a configuration path
+ * - a connection string
+ * 
+ * For details see {@link https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax Connection Syntax}.
  *
  * @param {} [dc]
  * Database Context.

lib/queryFile.js
@@ -26,8 +26,9 @@ var $npm = {
  * For any given SQL file you should only create a single instance of this class throughout the application.
  *
  * @param {string} file
- * Name/path of the SQL file with the query. If there is any problem reading the file, it will be
- * reported when executing the query.
+ * Path to the SQL file with the query, either absolute or relative to the application's entry point file.  
+ *
+ * If there is any problem reading the file, it will be reported when executing the query.
  *
  * @param {object} [options]
  * A set of configuration options.

lib/utils/public.js
@@ -345,9 +345,10 @@ function objectToCode(obj, cb) {
  * Configuration parameter for generating the code.
  *
  * - When it is a non-null object, it is assumed to be a configuration object (see the format above).
- * - When it is a text string - it is the relative path to a JSON file that contains the configuration object.
- * - When `config` isn't specified, the method will try to locate the default `sql-config.json` file in the same folder as
- *   your module's start-up file, and if not found - throw {@link external:Error Error} = `Default SQL configuration file not found`.
+ * - When it is a text string - it is the relative path to either a JSON file that contains the configuration object,
+ *   or a Node.js module that exports one. The path is relative to the application's entry point file.
+ * - When `config` isn't specified, the method will try to locate the default `sql-config.json` file in the directory of your
+ *   application's entry point file, and if not found - throw {@link external:Error Error} = `Default SQL configuration file not found`.
  *
  * @returns {string}
  * Generated code.
@@ -399,15 +400,12 @@ function objectToCode(obj, cb) {
  *
  */
 function buildSqlModule(config) {
-
-    // root = folder where the start-up js file is located
-    var root = path.dirname(process.argv[1]);
-
+    
     if (utils.isText(config)) {
-        config = require(path.join(root, config));
+        config = require(path.join(utils.startDir, config));
     } else {
         if (utils.isNull(config)) {
-            var defConfig = path.join(root, 'sql-config.json');
+            var defConfig = path.join(utils.startDir, 'sql-config.json');
             // istanbul ignore else;
             if (!fs.existsSync(defConfig)) {
                 throw new Error("Default SQL configuration file not found: " + defConfig);
@@ -506,3 +504,5 @@ module.exports = {
     objectToCode: objectToCode,
     buildSqlModule: buildSqlModule
 };
+
+Object.freeze(module.exports);

test/dbSpec.js
@@ -23,7 +23,7 @@ var $errors = {
 };
 
 describe("Database Instantiation", function () {
-    it("must throw an error when empty or no connection passed", function () {
+    it("must throw an invalid connection passed", function () {
         var err = "Invalid connection details.";
         expect(pgp).toThrow(err);
         expect(function () {
@@ -32,10 +32,19 @@ describe("Database Instantiation", function () {
         expect(function () {
             pgp("");
         }).toThrow(err);
+        expect(function () {
+            pgp(123);
+        }).toThrow(err);
     });
-    var testDB = pgp("invalid connection details");
-    it("must return a valid, though non-connectible object", function () {
-        expect(typeof(testDB)).toBe('object');
+    it("must throw on invalid file path", function () {
+        var err;
+        try {
+            pgp("invalid connection details");
+        } catch (e) {
+            err = e;
+        }
+        expect(err instanceof Error).toBe(true);
+        expect(err.message).toContain("Cannot find module");
     });
 });
 

commit 3b6c2fadecc1ff725a90dc7d9ed1bf274240e52a

fallback when run from Node interpreter.
This bug was introduced with 9ea9385. Since Node interpreter don't
have `process.argv[1]`, `path.dirname` would fail with the following
error:

    TypeError: Path must be a string. Received undefined

See: #158.

lib/utils/index.js
@@ -125,7 +125,7 @@ function isPathAbsolute(path) {
 }
 
 module.exports = {
-    startDir: $path.dirname(process.argv[1]),
+    startDir: $path.dirname(process.argv[1] || process.cwd()),
     isPathAbsolute: isPathAbsolute,
     lock: lock,
     isText: isText,