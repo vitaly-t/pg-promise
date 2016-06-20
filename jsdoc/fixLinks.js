8d0969b281083c382ca49b38b929d216c2a4234f
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-06-20 09:28:37 +0100

    extending type TableName.

--- jsdoc/fixLinks.js
+++ jsdoc/fixLinks.js
@@ -24,7 +24,8 @@ var links = {
     "ES6 generator": "https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/function*",
     "Performance Boost": "https://github.com/vitaly-t/pg-promise/wiki/Performance-Boost",
     "Prepared Statement": "http://www.postgresql.org/docs/9.5/static/sql-prepare.html",
-    "Prepared Statements": "http://www.postgresql.org/docs/9.5/static/sql-prepare.html"
+    "Prepared Statements": "http://www.postgresql.org/docs/9.5/static/sql-prepare.html",
+    "Custom Type Formatting": "https://github.com/vitaly-t/pg-promise#custom-type-formatting"
 };
 
 function fixLinks(source) {
