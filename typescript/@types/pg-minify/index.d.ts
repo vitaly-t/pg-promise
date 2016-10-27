129c15b65363bb2b3b3f295175fb5cb596b97c58
Mauricio Gemelli Vigolo <mauriciogv@sanepar.com.br>
2016-10-27 13:43:26 -0200

    typescript 2 support - @types

---
+++ typescript/@types/pg-minify/index.d.ts
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////
+// Module pg-minify that's used and exposed by pg-promise
+//
+// Supported version of pg-minify: 0.2.4 and later.
+//
+// pg-minify: https://github.com/vitaly-t/pg-minify
+//////////////////////////////////////////////////////////
+declare module 'pg-minify' {
+
+    interface IErrorPosition {
+        line: number,
+        column: number
+    }
+
+    namespace pgMinify {
+        enum parsingErrorCode {
+            unclosedMLC = 1,    // Unclosed multi-line comment.
+            unclosedText = 2,   // Unclosed text block.
+            unclosedQI = 3,     // Unclosed quoted identifier.
+            multiLineQI = 4     // Multi-line quoted identifiers are not supported.
+        }
+
+        class SQLParsingError implements Error {
+            name: string;
+            message: string;
+            stack: string;
+            error: string;
+            code: parsingErrorCode;
+            position: IErrorPosition;
+        }
+    }
+
+    function pgMinify(sql: string, options?: { compress?: boolean }): string;
+
+    export = pgMinify;
+}
