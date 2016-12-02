7ac6e44af8374713e5818d99123f8ae4602cc886
Vitaly Tomilov <vitaly.x.tomilov@aib.ie>
2016-12-02 16:33:02 +0000

    removing `canGenerate` test.

--- test/typescript/help.ts
+++ test/typescript/help.ts
@@ -62,8 +62,6 @@ var sets1 = pgp.helpers.sets({});
 var sets2 = pgp.helpers.sets([]);
 var sets3 = pgp.helpers.sets({}, cs);
 
-var test: boolean = cs.canGenerate(data);
-
 var cs1 = cs.extend(['']);
 var cs2 = cs1.merge(cs);
 
