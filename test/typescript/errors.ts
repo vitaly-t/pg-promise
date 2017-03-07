0e424e2b7e46dc84f96861ebbacd5018d404139b
Jarrad Whitaker <JWhitaker@officeworks.com.au>
2017-03-07 11:46:00 +1100

    change errors ts declaration

--- test/typescript/errors.ts
+++ test/typescript/errors.ts
@@ -1,12 +1,12 @@
 import * as pgPromise from '../../typescript/pg-promise';
 
-var err1 = <typeof pgPromise.errors.QueryResultError>null;
+var err1 = <pgPromise.errors.QueryResultError>null;
 var query = err1.query;
 
-var err2 = <typeof pgPromise.errors.QueryFileError>null;
+var err2 = <pgPromise.errors.QueryFileError>null;
 var line = err2.error.position.line;
 
-var err3 = <typeof pgPromise.errors.PreparedStatementError>null;
+var err3 = <pgPromise.errors.PreparedStatementError>null;
 var file = err3.error.file;
 
 var qrec = pgPromise.errors.queryResultErrorCode;
