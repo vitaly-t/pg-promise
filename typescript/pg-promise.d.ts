8b6e348aa6942a686ab81104e793cdacabe89360
rafaelkallis <rk@rafaelkallis.com>
2017-07-24 16:13:07 +0200

    types: added return type to task() and tx()

--- typescript/pg-promise.d.ts
+++ typescript/pg-promise.d.ts
@@ -419,15 +419,15 @@ declare namespace pgPromise {
 
         // Tasks
         // API: http://vitaly-t.github.io/pg-promise/Database.html#task
-        task(cb: (t: ITask<Ext> & Ext) => any): XPromise<any>
+        task<T>(cb: (t: ITask<Ext> & Ext) => T): XPromise<T>
 
-        task(tag: any, cb: (t: ITask<Ext> & Ext) => any): XPromise<any>
+        task<T>(tag: any, cb: (t: ITask<Ext> & Ext) => T): XPromise<T>
 
         // Transactions
         // API: http://vitaly-t.github.io/pg-promise/Database.html#tx
-        tx(cb: (t: ITask<Ext> & Ext) => any): XPromise<any>
+        tx<T>(cb: (t: ITask<Ext> & Ext) => T): XPromise<T>
 
-        tx(tag: any, cb: (t: ITask<Ext> & Ext) => any): XPromise<any>
+        tx<T>(tag: any, cb: (t: ITask<Ext> & Ext) => T): XPromise<T>
     }
 
     // Database object in connected state;
