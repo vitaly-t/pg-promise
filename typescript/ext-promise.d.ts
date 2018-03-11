009a940e957f2586672ea94f35ad1c7f41a7b4c4
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2018-03-11 20:38:13 +0000

    adding copyright into source files

--- typescript/ext-promise.d.ts
+++ typescript/ext-promise.d.ts
@@ -1,4 +1,13 @@
 /*
+ * Copyright (c) 2015-present, Vitaly Tomilov
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+/*
 
  External Promise Provider.
 
