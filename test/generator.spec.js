432f9bdc8215fe71330a60acae56b00e9e9f8632
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-07-25 08:31:46 +0100

    removing support for generators - #633

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -8,99 +8,44 @@ const options = {
 
 const dbHeader = header(options);
 const db = dbHeader.db;
-const pgp = dbHeader.pgp;
 
-describe('Generators - Positive', () => {
+describe('ES6 Generators', () => {
 
-    let result, tag, query;
+    // v9.0 dropped all generators support, and now it throws an error,
+    // if someone still tries to use them.
 
-    const mode = new pgp.txMode.TransactionMode({
-        tiLevel: pgp.txMode.isolationLevel.serializable
-    });
+    const errMsg = 'ES6 generator functions are no longer supported!';
 
-    beforeEach(done => {
-        options.transact = e => {
-            tag = e.ctx.tag;
-        };
-        options.query = e => {
-            if (!query) {
-                query = e.query;
-            }
-        };
-        db.tx({tag: 'Custom', mode}, function* (t) {
-            return yield t.one('select 123 as value');
-        })
-            .then(data => {
-                result = data;
-                done();
-            });
-    });
-
-    it('must resolve with the right value', () => {
-        expect(result && typeof result === 'object').toBeTruthy();
-        expect(result.value).toBe(123);
-        expect(tag).toBe('Custom');
-        expect(query).toBe('begin isolation level serializable');
-    });
-
-    afterEach(() => {
-        delete options.task;
-        delete options.query;
-    });
-
-});
-
-describe('Generators - Negative', () => {
-
-    describe('normal reject', () => {
-        let result;
-        const err = new Error('ops!');
-
-        const myTask = function* () {
-            return yield promise.reject(err);
-        };
-
+    describe('for tasks', () => {
+        let error;
         beforeEach(done => {
-            db.task(myTask)
-                .catch(error => {
-                    result = error;
+            db.task(function* () {
+            })
+                .catch(e => {
+                    error = e;
                 })
                 .finally(done);
         });
-
-        it('must reject with the right value', () => {
-            expect(result).toBe(err);
+        it('must throw error', () => {
+            expect(error instanceof TypeError).toBe(true);
+            expect(error.message).toBe(errMsg);
         });
     });
 
-    describe('error thrown', () => {
-
-        let result, tag;
-
-        // eslint-disable-next-line
-        function* myTask() {
-            throw 123;
-        }
-
+    describe('for transactions', () => {
+        let error;
         beforeEach(done => {
-            options.task = e => {
-                tag = e.ctx.tag;
-            };
-            db.task('myTag', myTask)
-                .catch(error => {
-                    result = error;
+            db.tx(function* () {
+            })
+                .catch(e => {
+                    error = e;
                 })
                 .finally(done);
         });
-
-        it('must reject with the right value', () => {
-            expect(result).toBe(123);
-            expect(tag).toBe('myTag');
+        it('must throw error', () => {
+            expect(error instanceof TypeError).toBe(true);
+            expect(error.message).toBe(errMsg);
         });
-
-        afterEach(() => {
-            delete options.task;
-        });
-
     });
+
 });
