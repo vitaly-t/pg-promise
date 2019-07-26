d9e5b5fd60558f6cfe39469343cd0fd9ba92f759
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-07-26 03:01:19 +0100

    adding type id-s

--- typescript/pg-subset.d.ts
+++ typescript/pg-subset.d.ts
@@ -13,7 +13,7 @@
 // Calling it 'pg-subset' to avoid a conflict in case the application also
 // includes the official 'pg' typings.
 //
-// Supported version of pg: 7.11.0 and later.
+// Supported version of pg: 7.12.0 and later.
 //
 // pg: https://github.com/brianc/node-postgres
 //////////////////////////////////////////////////////////////////////////////
@@ -100,11 +100,74 @@ declare namespace pg {
         keepalives_idle?: number
     }
 
+    enum TypeId {
+        BOOL = 16,
+        BYTEA = 17,
+        CHAR = 18,
+        INT8 = 20,
+        INT2 = 21,
+        INT4 = 23,
+        REGPROC = 24,
+        TEXT = 25,
+        OID = 26,
+        TID = 27,
+        XID = 28,
+        CID = 29,
+        JSON = 114,
+        XML = 142,
+        PG_NODE_TREE = 194,
+        SMGR = 210,
+        PATH = 602,
+        POLYGON = 604,
+        CIDR = 650,
+        FLOAT4 = 700,
+        FLOAT8 = 701,
+        ABSTIME = 702,
+        RELTIME = 703,
+        TINTERVAL = 704,
+        CIRCLE = 718,
+        MACADDR8 = 774,
+        MONEY = 790,
+        MACADDR = 829,
+        INET = 869,
+        ACLITEM = 1033,
+        BPCHAR = 1042,
+        VARCHAR = 1043,
+        DATE = 1082,
+        TIME = 1083,
+        TIMESTAMP = 1114,
+        TIMESTAMPTZ = 1184,
+        INTERVAL = 1186,
+        TIMETZ = 1266,
+        BIT = 1560,
+        VARBIT = 1562,
+        NUMERIC = 1700,
+        REFCURSOR = 1790,
+        REGPROCEDURE = 2202,
+        REGOPER = 2203,
+        REGOPERATOR = 2204,
+        REGCLASS = 2205,
+        REGTYPE = 2206,
+        UUID = 2950,
+        TXID_SNAPSHOT = 2970,
+        PG_LSN = 3220,
+        PG_NDISTINCT = 3361,
+        PG_DEPENDENCIES = 3402,
+        TSVECTOR = 3614,
+        TSQUERY = 3615,
+        GTSVECTOR = 3642,
+        REGCONFIG = 3734,
+        REGDICTIONARY = 3769,
+        JSONB = 3802,
+        REGNAMESPACE = 4089,
+        REGROLE = 4096
+    }
+
     // Interface of 'pg-types' module;
     // See: https://github.com/brianc/node-pg-types
     interface ITypes {
-        setTypeParser: (oid: number, format: string | ((value: string) => any)) => void
-        getTypeParser: (oid: number, format?: string) => any
+        setTypeParser: (id: TypeId, format: string | ((value: string) => any)) => void
+        getTypeParser: (id: TypeId, format?: string) => any
         arrayParser: (source: string, transform: (entry: any) => any) => any[]
     }
 
@@ -169,12 +232,8 @@ declare namespace pg {
         keepalives_idle: number
     }
 
-    class Connection {
-        // not needed within pg-promise;
-    }
-
     class Query {
-        // not needed within pg-promise;
+        // not used within pg-promise;
     }
 
     class Client extends EventEmitter {
