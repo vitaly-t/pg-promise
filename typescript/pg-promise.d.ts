ef7483b00af542efe2eab81da72215b6e3d0dc62
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-11-11 14:08:23 +0000

    upp the version

--- typescript/pg-promise.d.ts
+++ typescript/pg-promise.d.ts
@@ -8,7 +8,7 @@
  */
 
 /////////////////////////////////////////
-// Requires pg-promise v10.1.0 or later.
+// Requires pg-promise v10.2.0 or later.
 /////////////////////////////////////////
 
 // We use ES6 as static promise here, because generic promises are still not supported.
